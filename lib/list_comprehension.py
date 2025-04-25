30e0a0e77dba33bf5635a21d83935bcfc086ab2f
Fiona <fionakanyi0@gmail.com>
2025-04-25 15:40:41 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    """
-    Returns a list of even numbers from the input list.
-
-    Parameters:
-    num_list (list of int): A list of integers.
-
-    Returns:
-    list of int: A list containing only even numbers from num_list.
-    """
     return [n for n in num_list if n % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
